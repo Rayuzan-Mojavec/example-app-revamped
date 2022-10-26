d293c22e57fd236aaf790022d18b0988855f9c20
Rayuzan-Mojavec <rayuzanmojavec@gmail.com>
2022-10-26 14:32:15 +0700

    hai

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,4 +8,11 @@
             </div>
         </div>
     </div>
+    <div class="my-24 flex justify-center">
+        <div class="group rounded-full">
+            <div class="rounded-full p-32 bg-gradient-to-r from-green-400 to-orange-500 text-center group-hover:animate-spin">
+                <p class="text-2xl font-bold"></p>
+            </div>
+        </div>
+    </div>
 @endsection
